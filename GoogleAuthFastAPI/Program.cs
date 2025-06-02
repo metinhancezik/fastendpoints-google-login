63c21646808ab49e0d165e2cdfabbcf279751b67
Metinhan <126672425+metinhancezik@users.noreply.github.com>
2025-06-02 19:20:25 +0300

    Google Auth branch: setup (#1)

--- GoogleAuthFastAPI/Program.cs
+++ GoogleAuthFastAPI/Program.cs
@@ -1,6 +1,59 @@
+﻿using FastEndpoints;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authentication.Google;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
+
 var builder = WebApplication.CreateBuilder(args);
+
+//FastEndpoints
+builder.Services.AddFastEndpoints();
+builder.Services.AddAuthentication(options =>
+{
+    // Tarayıcı yönlendirmesi için cookie kullanılması gerekli
+    options.DefaultScheme = CookieAuthenticationDefaults.AuthenticationScheme;
+    options.DefaultChallengeScheme = GoogleDefaults.AuthenticationScheme;
+})
+.AddCookie() 
+.AddGoogle(options =>
+{
+    options.ClientId = builder.Configuration["Authentication:Google:ClientId"];
+    options.ClientSecret = builder.Configuration["Authentication:Google:ClientSecret"];
+})
+.AddJwtBearer(options =>
+{
+    //API isteklerinde JWT token doğrulama kuralları belirliyoruz
+    options.TokenValidationParameters = new TokenValidationParameters
+    {
+        ValidateIssuer = true,
+        ValidateAudience = true,
+        ValidateLifetime = true,
+        ValidateIssuerSigningKey = true,
+        ValidIssuer = builder.Configuration["Authentication:Jwt:Issuer"],
+        ValidAudience = builder.Configuration["Authentication:Jwt:Audience"],
+        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(builder.Configuration["Authentication:Jwt:Key"]))
+    };
+});
+
+builder.Services.AddAuthorization();
+
+builder.Services.ConfigureApplicationCookie(options =>
+{
+    options.Cookie.SameSite = SameSiteMode.None;
+    options.Cookie.SecurePolicy = CookieSecurePolicy.Always;
+});
+
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+if (app.Environment.IsDevelopment())
+{
+    app.UseDeveloperExceptionPage();
+}
+
+app.UseHttpsRedirection();
+app.UseAuthentication(); 
+app.UseAuthorization();
+app.UseFastEndpoints();
 
 app.Run();
